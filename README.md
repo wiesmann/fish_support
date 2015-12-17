5ad573f1d4bb2e80afa2f6a67c472df05977cce1
Matthias Wiesmann <matthias.wiesmann@gmail.com>
2015-12-17 07:04:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fish Support
+
+Scripts to support and configure the [fish shell](https://github.com/fish-shell/fish-shell/) on Mac OS X.
+
+## Alias configuration
+
+The script `applications_to_fish_functions.py` creates a `fish` function for each Mac OS X application. It also populates the description field.
+
+![Screenshot](screenshot.png "tab completion for te*")
+
+To use it, first generate functions for all Mac OS X applications
+
+```shell
+./applications_to_fish_functions.py > ~/.config/fish/app_functions
+```
+
+Then source that snippet from your fish configuration.
+
+```shell
+if test -f ~/.config/fish/app_functions
+  . ~/.config/fish/app_functions
+end
+```
---
+++ applications_to_fish_functions.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+"""Build the list of fish aliases/commands from all mac os x Applications."""
+
+__author__ = 'matthias.wiesmann@a3.epfl.ch (Matthias Wiesmann)'
+
+import itertools
+import operator
+import os
+import re
+import subprocess
+import sys
+
+ENCODING = os.getenv('LANG', 'en_US.UTF-8').split('.')[1].lower()
+MDFIND = ('/usr/bin/mdfind', '-0', 'kMDItemKind == Application')
+MDLS = (
+    '/usr/bin/mdls',
+    '--name=kMDItemAppStoreCategory',
+    '--name=kMDItemDisplayName',
+    '--raw')
+FUNCTION_TEMPLATE = """function %(command_name)s -d "%(description)s"
+  /usr/bin/open -a %(application_path)s $argv
+end
+"""
+
+def GetAllApps():
+  result = subprocess.check_output(MDFIND).decode(ENCODING)
+  return result.split('\0')
+
+
+def GetDescription(paths):
+  for path in paths:
+    command = itertools.chain(MDLS, [path])
+    result = subprocess.check_output(command).decode(ENCODING)
+    app_type, name,  = result.split('\0')
+    if app_type != '(null)':
+      description = '%s (%s)' % (name, app_type)
+    else:
+      description = name
+    yield path, description
+
+
+def FilterPath(path):
+  if path.startswith('/System'):
+    return False
+  if path.startswith('/Library'):
+    return False
+  if not path.endswith('.app'):
+    return False
+  return True
+
+
+def MakeCommandName(path):
+  name = os.path.basename(path).lower()
+  return '_'.join(re.split(r'[().;!?,\s]', name))
+
+def EscapePath(path):
+  return re.escape(path).replace('\/', '/')
+
+if __name__ == '__main__':
+
+  good_paths = itertools.ifilter(FilterPath, GetAllApps())
+  for path, description in GetDescription(good_paths):
+    args = {
+      'description': description,
+      'command_name': MakeCommandName(path),
+      'application_path': EscapePath(path),
+    }
+    func_text = FUNCTION_TEMPLATE % args
+    print func_text.encode('utf-8')
+
